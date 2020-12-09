767adf5cc2bbde5c1fb9439bfa60887108afc840
ProgramistaZpolski <piotrbadelek@protonmail.com>
2020-12-09 12:24:31 +0100

    semicolons for the win

--- sfb.js
+++ sfb.js
@@ -1,8 +1,8 @@
 setInterval(function(){
-let h = ["h1", "i" ,"span","p", "h2", "h3", "h4", "span", "b", "a", "h5", "h6"]
+let h = ["h1", "i" ,"span","p", "h2", "h3", "h4", "span", "b", "a", "h5", "h6"];
 h.forEach(function(a) {
     document.querySelectorAll(a).forEach(hh => {
-        hh.innerText = "h"
+        hh.innerText = "h";
     })
 })
 }, 2500);
