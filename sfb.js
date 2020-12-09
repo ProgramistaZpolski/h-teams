5c169bedefcd22c46762b30317936ea5b423041f
ProgramistaZpolski <piotrekbadelek@gmail.com>
2020-12-09 12:22:28 +0100

    Add files via upload

---
+++ sfb.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+let h = ["h1", "i" ,"span","p", "h2", "h3", "h4", "span", "b", "a", "h5", "h6"]
+h.forEach(function(a) {
+    document.querySelectorAll(a).forEach(hh => {
+        hh.innerText = "h"
+    })
+})
+}, 2500);
